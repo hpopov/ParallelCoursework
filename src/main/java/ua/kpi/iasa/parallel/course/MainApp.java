259fb33f3217b0875b74b700e20a0b05cbe96e05
Hryhorii_Popov <Hryhorii_Popov@EPUAKYIW2850.kyiv.epam.com>
2019-05-21 23:55:24 +0300

    Integrate javaFX with jzy3d framework using fxml
    - create basic controller for the future usage;
    - create chart drawing stub for demo purposes;
    - remove unused autogenerated classes

--- src/main/java/ua/kpi/iasa/parallel/course/MainApp.java
+++ src/main/java/ua/kpi/iasa/parallel/course/MainApp.java
@@ -1,36 +1,36 @@
 package ua.kpi.iasa.parallel.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MainApp extends Application {
 
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+	private static final Logger log = LoggerFactory.getLogger(MainApp.class);
 
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
+	public static void main(String[] args) throws Exception {
+		launch(args);
+	}
 
-    public void start(Stage stage) throws Exception {
+	public void start(Stage stage) throws Exception {
+		log.info("Starting application");
+		String fxmlFile = "/fxml/plot.fxml";
+		log.debug("Loading FXML for main view from: {}", fxmlFile);
+		FXMLLoader loader = new FXMLLoader();
+		Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
+		log.debug("Showing JFX scene");
+		Scene scene = new Scene(rootNode, 500, 500);
+		scene.getStylesheets().add("/styles/styles.css");
 
-        String fxmlFile = "/fxml/hello.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-        log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
-        scene.getStylesheets().add("/styles/styles.css");
-
-        stage.setTitle("Hello JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
-    }
+		PlotController plotController = loader.getController();
+		plotController.addSceneSizeChangedListener(scene);
+		stage.setTitle("Parallel calculation coursework presentation");
+		stage.setScene(scene);
+		stage.show();
+	}
 }
